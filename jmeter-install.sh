e7d11c1f52b04669d2d9b9674873205936b43f4a
Jorge Tressino Rua <jorge.rua@live.com>
2017-07-07 18:16:24 -0300

    Initial install script

---
+++ jmeter-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#java install
+apt-get install -y default-jre
+
+#jmeter 3.2 download
+wget http://mirror.nexcess.net/apache//jmeter/binaries/apache-jmeter-3.2.tgz
+tar -xf apache-jmeter-3.2.tgz
+mv apache-jmeter-3.2/ /usr/share
+
+#jmeter properties adjustment
+sed -i 's/#server.rmi.localport/server.rmi.localport/g' /usr/share/apache-jmeter-3.2/bin/jmeter.properties
+
+sh /usr/share/apache-jmeter-3.2/bin/jmeter.sh -s &
