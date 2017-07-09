d676f55eaf9d23e49369034f0b84e3a4a00ccfd4
Jorge Tressino Rua <jorge.rua@live.com>
2017-07-08 21:50:36 -0300

    Jmeter master script added

---
+++ jmeter-install-master.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#java install
+apt-get install -y default-jre
+
+#jmeter 3.2 download
+wget http://mirror.nexcess.net/apache//jmeter/binaries/apache-jmeter-3.2.tgz
+tar -xf apache-jmeter-3.2.tgz
+mv apache-jmeter-3.2/ /usr/share
